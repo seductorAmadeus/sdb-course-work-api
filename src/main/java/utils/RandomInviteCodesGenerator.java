a7b1d89b61d48cf11297998d3b1072a8277e2e6f
seductorAmadeus <martinraila@mail.ru>
2017-12-23 07:09:59 +0300

    Added registration codes generator

---
+++ src/main/java/utils/RandomInviteCodesGenerator.java
@@ -0,0 +1,47 @@
+package utils;
+
+import daos.RegistrationCodesDAO;
+import entities.RegistrationCodes;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomInviteCodesGenerator {
+
+    private final static int INVITE_CODE_LENGTH = 38;
+    // точность как гарантия рандома
+    private final static int INVITE_CODES_COUNT = 10;
+    private BigDecimal inviteCode;
+
+    private BigDecimal getNonExistsInviteCode(List<BigDecimal> inviteCodeList) {
+        BigDecimal inviteCode = null;
+        RegistrationCodesDAO registrationCodesDAO = new RegistrationCodesDAO();
+
+        // получаем список имеющихся инвайт-кодов
+        List<RegistrationCodes> registrationCodesList = registrationCodesDAO.listRegistrationCodes();
+
+        for (BigDecimal decimal : inviteCodeList) {
+            if (!registrationCodesList.contains(decimal)) {
+                inviteCode = decimal;
+            }
+        }
+
+        return inviteCode;
+    }
+
+    public BigDecimal getInviteCode() {
+        List<BigDecimal> inviteCodesList = new ArrayList<>();
+        for (int i = 0; i < INVITE_CODES_COUNT; i++) {
+            long randomNum = ThreadLocalRandom.current().nextInt(INVITE_CODE_LENGTH, Integer.MAX_VALUE);
+            inviteCodesList.add(BigDecimal.valueOf(randomNum));
+        }
+        try {
+            inviteCode = getNonExistsInviteCode(inviteCodesList);
+        } catch (NullPointerException exp) {
+            exp.getMessage();
+        }
+        return inviteCode;
+    }
+}
